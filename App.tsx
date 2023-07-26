f4f213004abb92fcb99ae811c4710ea748ddf67f
dom000 <godknowsegiboy00@gmail.com>
2023-07-26 14:32:28 +0100

    .

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <View className="flex-1 items-center justify-center bg-white">
-        <Text>Open up App.tsx jj to start working on your app!</Text>
         <StatusBar style="auto" />
+        <MapView style={styles.map}>
+          <Text className="absolute">
+            Open up App.tsx jj to start working on your app!
+          </Text>
+        </MapView>
       </View>
     </SafeAreaProvider>
   );
@@ -16,8 +21,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
